65e500b03cbaea7ae02bd170d3330990b3892fc0
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-19 16:20:18 +0200

    Refactor KF interface, make X public.

--- include/EKF.h
+++ include/EKF.h
@@ -6,7 +6,7 @@
 #include <memory>
 
 /**
- *  Linear Kalman Filter
+ *  Extended (or Linear) Kalman Filter
  *  Equations: https://www.kalmanfilter.net/multiSummary.html
  *  Update equations:
  *      x = F * x + G * u
@@ -15,15 +15,16 @@
  *      K = P * Ht * (H * P * Ht + R).inv()
  *      x = x + K * (z - H * x)
  *      P = (I - K * H) * P * (I - K * H)t + K * R * Kt
+ *  Filter is linear or extended, depending on the input model to the filter.
  */
-class LKF : public IKalmanFilter {
+class EKF : public IKalmanFilter {
 public:
   using MPtr = std::unique_ptr<IModel>;
 
-  explicit LKF(MPtr pModel) : pM{std::move(pModel)} {
-    auto Sz = pM->H.rows();
-    auto Sx = pM->H.cols();
-    auto Su = pM->G.cols();
+  explicit EKF(MPtr pModel) : pM{std::move(pModel)} {
+    auto Sz = pM->H().rows();
+    auto Sx = pM->H().cols();
+    auto Su = pM->G().cols();
     X = VectorXd::Zero(Sx);
     P = MatrixXd::Identity(Sx, Sx);
     Q = MatrixXd::Identity(Sx, Sx);
@@ -43,8 +44,6 @@ private:
   MatrixXd R;  // Measurement Uncertainty
   MatrixXd K;  // Kalman Gain
   MatrixXd I;  // Identity Matrix
-
-  VectorXd X;  // Current State Vector
 };
 
 #endif //KALMAN_FILTERS_CPP_EKF_H
--- include/KF.h
+++ include/KF.h
@@ -12,10 +12,24 @@ class IKalmanFilter {
 public:
   virtual ~IKalmanFilter() = default;
 
+  /**
+   * Update Kalman Filter
+   * @param z measurement
+   * @param u input
+   * @param dt time step
+   */
   virtual void update(const VectorXd& z, const VectorXd& u, double dt) = 0;
 
-  /// Returns the estimated state
+  /**
+   * Estimates the next state
+   * @param u input
+   * @param dt time step
+   * @return next state
+   */
   virtual VectorXd predict(const VectorXd& u, double dt) = 0;
+
+  /** Current State Vector */
+  VectorXd X;
 };
 
 #endif //SELF_03_KALMAN_FILTERS_CPP_KF_H
