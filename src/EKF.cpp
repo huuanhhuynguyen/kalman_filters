d17efc59673b12ae8bbc5c36e5c4e343eb27782b
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-20 16:10:25 +0200

    Refactor models.

--- src/EKF.cpp
+++ src/EKF.cpp
@@ -1,23 +1,22 @@
 #include "EKF.h"
+#include <iostream>
 
-void EKF::update(const VectorXd &z, const VectorXd& u, double dt)
-{
+void EKF::update(const VectorXd &z, const VectorXd& u, double dt) {
   pM->update(dt, X);
-  MatrixXd H = pM->H();
+  MatrixXd J_h = pM->J_h();
 
-  K = P * H.transpose() * (H * P * H.transpose() + R).inverse();
-  X = X + K * (z - H * X);
-  P = (I - K * H) * P * (I - K * H).transpose() + K * R * K.transpose();
+  K = P * J_h.transpose() * (J_h * P * J_h.transpose() + R).inverse();
+  X = X + K * (z - pM->h(X));
+  P = (I - K * J_h) * P * (I - K * J_h).transpose() + K * R * K.transpose();
 }
 
 VectorXd EKF::predict(const VectorXd& u, double dt)
 {
   pM->update(dt, X);
-  MatrixXd F = pM->F();
-  MatrixXd G = pM->G();
+  MatrixXd J_f = pM->J_f();
 
-  X = F * X + G * u;
-  P = F * P * F.transpose() + Q;
+  X = pM->f(X) + pM->g(u);
+  P = J_f * P * J_f.transpose() + Q;
   return X;
 }
 
