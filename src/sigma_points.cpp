65d62f19e49fafc88af44034326aa026d5934d79
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-29 11:26:12 +0200

    Edit error handling & messages.

--- src/sigma_points.cpp
+++ src/sigma_points.cpp
@@ -4,21 +4,21 @@
 MatrixXd compute_sigma_points(const VectorXd& muy, MatrixXd cov)
 {
   if (cov.rows() != cov.cols()) {
-    throw(std::invalid_argument("Error: cov must be a square matrix."));
+    throw(std::invalid_argument("Error: 'cov' must be a square matrix."));
   }
 
   if (muy.size() != cov.rows()) {
-    throw(std::invalid_argument("Error: muy and cov must have the same number of rows."));
+    throw(std::invalid_argument("Error: 'muy' and 'cov' must have the same number of rows."));
   }
 
   for (int i = 0; i < cov.rows(); ++i) {
     if (cov(i, i) <= 0.0) {
-      throw(std::invalid_argument("Error: diagonal elements of cov must be non-negative"));
+      throw(std::invalid_argument("Error: diagonal elements of 'cov' must be non-negative"));
     }
   }
 
   // cov = sqrt( (n + lambda) * cov )
-  // Note that: square-root of matrix A is vector S if A = S * S.transpose()
+  // Note: square-root of a matrix A is the vector S if A = S * S.transpose()
   {
     const int n = cov.rows();
     const int lambda = 3 - n;
@@ -26,7 +26,12 @@ MatrixXd compute_sigma_points(const VectorXd& muy, MatrixXd cov)
     SelfAdjointEigenSolver<MatrixXd> estimator(cov);
     const auto sqrt = estimator.operatorSqrt();
     //const MatrixXd sqrt = cov.llt().matrixL();
-    assert((cov - sqrt*sqrt.transpose()).norm() < 1e-9);
+    if ((cov - sqrt*sqrt.transpose()).norm() > 1e-9) {
+      throw(std::logic_error("Error: Either the calculation of square-root of "
+                            "covariance matrix 'cov' is wrong or the given "
+                            "'cov' has a problem. In the second case, check"
+                            "your UKF equations or UKF model pointer member."))
+    }
     cov = sqrt;
   }
 
@@ -48,7 +53,7 @@ MatrixXd compute_sigma_points(const VectorXd& muy, MatrixXd cov)
   }
   mean /= (2 * n + 1);
   if ((mean - muy).norm() > 1e-9) {
-    throw(std::logic_error("Mean of all sigma points must equal muy."));
+    throw(std::logic_error("Mean of all sigma points must equal 'muy'."));
   }
 
   return sigma;
